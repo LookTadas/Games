a87f3c78bdbc50ee6a5c125a4abda52972d0db56
looktadas <look.tadas@gmail.com>
2022-09-25 20:47:22 -0700

    modified:   snake/pages/header.php

--- snake/pages/header.php
+++ snake/pages/header.php
@@ -9,10 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="" method ="get">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="" method ="get">Snake Game</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
@@ -26,9 +23,6 @@
                         <a class="dropdown-item" href="#">Something else here</a>
                     </div>
                 </li>
-                <!-- <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li> -->
             </ul>
         </div>
     </nav>

2b8bd5e969acdc371ea82b2c1ffb69dfa5510b24
looktadas <look.tadas@gmail.com>
2022-09-25 20:58:00 -0700

    modified:   snake/css/main.css modified:   snake/pages/gameOver.php

--- snake/pages/gameOver.php
+++ snake/pages/gameOver.php
@@ -1,8 +1,10 @@
+
+
 <?php {?>
 <div class="form">
     <form action="" method="post">
         <input type="text" name="playerName" placeholder="Player name">
-        <input type="text" name="tag" placeholder="player tag">
+        <input type="text" name="tag" placeholder="player tag" maxlength="3" style = "text-transform: uppercase">
         <input type="submit" name="save" value="save" id="button">
     </form>
 </div>
